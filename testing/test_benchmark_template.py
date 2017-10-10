e74e8f40e6f750d4beeb430898bfdce1e23b0cc5
Conroy Whitney <conroy.whitney@gmail.com>
2017-10-09 20:39:14 -0800

    Add `index.html` template, example `building_list.yaml`, and output

--- testing/test_benchmark_template.py
+++ testing/test_benchmark_template.py
@@ -4,8 +4,23 @@ template.  It creates an output HTML file at the path: output/sample.html.
 import yaml
 from jinja2 import Template
 
-sample_data = yaml.load(open('sample_benchmark_data.yaml').read())
+buildings = yaml.load(open('sample_buildings_list.yaml').read())
+sample_building = yaml.load(open('sample_benchmark_data.yaml').read())
 
+"""Example
 template = Template(open('../templates/example.html').read())
-result = template.render(sample_data)
+result = template.render(sample_building)
 open('output/example.html', 'w').write(result)
+"""
+
+# TODO: Give template a list of buildings
+template = Template(open('../templates/index.html').read())
+result = template.render(buildings)
+open('output/index.html', 'w').write(result)
+
+"""
+# TODO: Loop through each building and render template
+template = Template(open('../templates/building.html').read())
+result = template.render(sample_building)
+open('output/big-dipper.html', 'w').write(result)
+"""
