79fdea8446d7446c99d12b9576dca0da277b4c0d
Conroy Whitney <conroy.whitney@gmail.com>
2017-10-09 20:13:32 -0800

    Create `templates` folder & rename `example.html`

--- testing/test_benchmark_template.py
+++ testing/test_benchmark_template.py
@@ -5,6 +5,7 @@ import yaml
 from jinja2 import Template
 
 sample_data = yaml.load(open('sample_benchmark_data.yaml').read())
-template = Template(open('../benchmark.html').read())
+
+template = Template(open('../templates/example.html').read())
 result = template.render(sample_data)
-open('output/sample.html', 'w').write(result)
+open('output/example.html', 'w').write(result)
