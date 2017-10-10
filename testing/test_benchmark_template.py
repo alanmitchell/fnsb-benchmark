c2e2ea7fbfdeff5c8bc364d384a70f1ff3499109
Conroy Whitney <conroy.whitney@gmail.com>
2017-10-09 21:14:10 -0800

    Add building-id subfolder with `index.html` root file

--- testing/test_benchmark_template.py
+++ testing/test_benchmark_template.py
@@ -2,25 +2,45 @@
 template.  It creates an output HTML file at the path: output/sample.html.
 """
 import yaml
+import os
 from jinja2 import Template
 
-buildings = yaml.load(open('sample_buildings_list.yaml').read())
-sample_building = yaml.load(open('sample_benchmark_data.yaml').read())
+sample_building = yaml.load(open("sample_benchmark_data.yaml").read())
+template_folder = "../templates/"
+buildings_template_folder = template_folder + "building/"
+output_folder = "output/"
 
 """Example
-template = Template(open('../templates/example.html').read())
+template = Template(open("../templates/example.html").read())
 result = template.render(sample_building)
-open('output/example.html', 'w').write(result)
+open("output/example.html", "w").write(result)
 """
 
 # TODO: Give template a list of buildings
-template = Template(open('../templates/index.html').read())
+buildings = yaml.load(open("sample_buildings_list.yaml").read())
+template = Template(open(template_folder + "index.html").read())
 result = template.render(buildings)
-open('output/index.html', 'w').write(result)
+open("output/index.html", "w").write(result)
 
-"""
 # TODO: Loop through each building and render template
-template = Template(open('../templates/building.html').read())
-result = template.render(sample_building)
-open('output/big-dipper.html', 'w').write(result)
-"""
+building_id = "big-dipper" # TODO: This will be determined by the loop
+building_dir = output_folder + building_id
+report_pages = ['index'] # TODO: Add more pages here
+for page in report_pages:
+    # just easier than appending '.html' to all of the report array items above
+    report_page_filename = page + ".html"
+
+    # template filename matches output filename just in different directory
+    template_filename = buildings_template_folder + report_page_filename
+
+    # read Jinja2 template
+    template = Template(open(template_filename).read())
+
+    # render the Jinja2 template with the building info
+    result = template.render(sample_building)
+
+    # ensure directory exists before we try writing file
+    os.makedirs(building_dir, 0o777, True)
+
+    # write the contents of the rendered report to the corresponding file
+    open(building_dir + "/" + report_page_filename, "w").write(result)
