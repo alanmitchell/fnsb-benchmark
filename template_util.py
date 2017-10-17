76536382e65f3a5bdbc05bbb57350920965f624e
Alan Mitchell <tabb99@gmail.com>
2017-10-17 10:06:46 -0800

    Updated template_util with newest custom filters.

--- template_util.py
+++ template_util.py
@@ -3,6 +3,7 @@ used by the benchmarking script.
 """
 
 from jinja2 import Environment, FileSystemLoader
+import numpy as np
 
 # Create the Template environment
 env = Environment(
@@ -12,52 +13,38 @@ env = Environment(
 #------ Below are Custom Filters used for Formatting --------
 
 def is_blank(val):
-    return (val is None) or (val == "nan")
+    try:
+        return not np.isfinite(val)
+    except:
+        return True
 
 def filter_blank(val):
-    if is_blank(val):
-        return ''
-    else:
-        return val
+    if is_blank(val): return ''
+    return val
 
-def filter_money(val):
-    try:
-        return "${:,}".format(val)
-    except TypeError:
-        return ''
-    except ValueError:
-        return val
+def filter_money(val, precision = 0):
+    if is_blank(val): return ''
+    format_string = "${:,.%sf}" % precision
+    return format_string.format(val)
 
-def filter_money_accurate(val):
-    try:
-        return "${:,.3f}".format(val)
-    except TypeError:
-        return ''
-    except ValueError:
-        return val
+def filter_number(val, precision = 0):
+    if is_blank(val): return ''
+    format_string = "{:,.%sf}" % precision
+    return format_string.format(val)
 
-def filter_number(val):
-    try:
-        return '{:,}'.format(val)
-    except TypeError:
-        return ''
-    except ValueError:
-        return val
+def filter_percent(val, precision = 1):
+    if is_blank(val): return ''
+    format_string = "{:.%s%%}" % precision
+    return format_string.format(val)
 
-def filter_percent(val):
-    try:
-        return "{:.0%}".format(val)
-    except TypeError:
-        return ''
-    except ValueError:
-        return val
-
-# Assign the custom filters to the Environment
+# -------------- Assign the custom filters to the Environment
+    
 env.filters['blank'] = filter_blank
 env.filters['money'] = filter_money
-env.filters['money_accurate'] = filter_money_accurate
 env.filters['number'] = filter_number
 env.filters['percent'] = filter_percent
+
+# ------------- Utility Functions -----------------
 
 def get_template(template_filename):
     """Returns a template having the name 'template_file_name'
