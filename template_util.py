57af4c2f66c8a498ec2fe31c9e7e56408d8c0879
Alan Mitchell <tabb99@gmail.com>
2017-10-16 21:08:57 -0800

    Added module to handle Template tasks.

---
+++ template_util.py
@@ -0,0 +1,65 @@
+"""Utilities and initialization code related to the Jinja2 templates
+used by the benchmarking script.
+"""
+
+from jinja2 import Environment, FileSystemLoader
+
+# Create the Template environment
+env = Environment(
+    loader=FileSystemLoader(['templates', 'templates/building'])
+)
+
+#------ Below are Custom Filters used for Formatting --------
+
+def is_blank(val):
+    return (val is None) or (val == "nan")
+
+def filter_blank(val):
+    if is_blank(val):
+        return ''
+    else:
+        return val
+
+def filter_money(val):
+    try:
+        return "${:,}".format(val)
+    except TypeError:
+        return ''
+    except ValueError:
+        return val
+
+def filter_money_accurate(val):
+    try:
+        return "${:,.3f}".format(val)
+    except TypeError:
+        return ''
+    except ValueError:
+        return val
+
+def filter_number(val):
+    try:
+        return '{:,}'.format(val)
+    except TypeError:
+        return ''
+    except ValueError:
+        return val
+
+def filter_percent(val):
+    try:
+        return "{:.0%}".format(val)
+    except TypeError:
+        return ''
+    except ValueError:
+        return val
+
+# Assign the custom filters to the Environment
+env.filters['blank'] = filter_blank
+env.filters['money'] = filter_money
+env.filters['money_accurate'] = filter_money_accurate
+env.filters['number'] = filter_number
+env.filters['percent'] = filter_percent
+
+def get_template(template_filename):
+    """Returns a template having the name 'template_file_name'
+    """
+    return env.get_template(template_filename)
