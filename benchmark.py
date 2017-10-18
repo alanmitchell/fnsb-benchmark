5431fbd468130c6294d07d2a961294734a0426b6
dmadden <dustin@cchrc.org>
2017-10-18 15:16:16 -0800

    Fixed "Sewer' Missing from Index issue

--- benchmark.py
+++ benchmark.py
@@ -549,15 +549,13 @@ def analyze_site(site, df, ut, report_date_time):
     # Shorten the utility list to include only energy-related columns
     utility_list = list(set(utility_list) - set(['sewer', 'water', 'refuse']))
     
-    p5g1_filename, p5g1_url = gu.graph_filename_url(site, "energy_usage_cost_g1")
-    gu.usage_pie_charts(usage_df2.fillna(0.0), usage_cols, 1, p5g1_filename)
+    pie_urls = gu.usage_pie_charts(usage_df2.fillna(0.0), usage_cols, 1, 'energy_usage_pie', site)
     
-    p5g2_filename, p5g2_url = gu.graph_filename_url(site, "energy_usage_cost_g2")
-    gu.usage_pie_charts(df2.fillna(0.0), utility_list, 2, p5g1_filename)
+    # Make the other graphs and append the URLs
+    pie_urls += gu.usage_pie_charts(df2.fillna(0.0), utility_list, 2, 'energy_cost_pie', site)
 
     # Add pie charts to template dictionary
-    template_data['energy_cost_usage'] = dict(
-        graphs=[p5g1_url])
+    template_data['energy_cost_usage'] = dict(graphs=pie_urls)
 
     # -------------------- Electrical Usage Analysis -------------------------
 
@@ -1017,18 +1015,29 @@ def analyze_site(site, df, ut, report_date_time):
                                   aggfunc=np.sum
     )
 
+    
+    # Add in columns for the missing services
+    water_missing_services = bu.missing_services(water_cost_df.columns)
+    bu.add_columns(water_cost_df, water_missing_services)
+    
     # Calculate totals, percent change
     water_cost_df = water_cost_df[water_cost_df.columns.difference(['Electricity', 'Natural Gas', 'Oil #1', 'Steam', 'Refuse'])]
     
-    water_cost_df = water_cost_df.rename(columns={'Sewer': 'Sewer Cost',
-                                                 'Water': 'Water Cost'})
-    water_cost_df['total_water_sewer_cost'] = water_cost_df['Sewer Cost'] + water_cost_df['Water Cost']
+    rename_dict = {'Sewer': 'Sewer Cost',
+                   'Water': 'Water Cost'}
+    
+    water_cost_df = water_cost_df.rename(columns=rename_dict)
+    
+    # First check to make sure sewer data is included; if so, calculate total cost
+    water_cost_df['total_water_sewer_cost'] = water_cost_df.sum(axis=1)
+    
     water_cost_df['water_cost_pct_change'] = water_cost_df['Water Cost'].pct_change()
     water_cost_df['sewer_cost_pct_change'] = water_cost_df['Sewer Cost'].pct_change()
+                                                              
     water_cost_df['total_water_sewer_cost_pct_change'] = water_cost_df.total_water_sewer_cost.pct_change()
 
     # Merge use and cost dataframes
-    water_use_and_cost = pd.merge(water_cost_df, water_gal_df, left_index=True, right_index=True, how='outer')
+    water_use_and_cost = pd.merge(water_cost_df, water_gal_df, left_index=True, right_index=True, how='left')
 
     water_use_and_cost['water_unit_cost'] = water_use_and_cost.total_water_sewer_cost / water_use_and_cost.Water
     water_use_and_cost['water_unit_cost_pct_change'] = water_use_and_cost.water_unit_cost.pct_change()
