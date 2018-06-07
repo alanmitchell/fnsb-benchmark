4839732d8fdd5890d309c4e184bb35a36ff2e4c5
Alan Mitchell <tabb99@gmail.com>
2018-06-07 12:10:12 -0800

    Finalized script to make site.

--- make_site.sh
+++ make_site.sh
@@ -18,12 +18,12 @@ EOF
 	exit 1
 }
 
-#cd /home/ahfc/fnsb-benchmark
-#python3.6 -u read_aris.py > logs/read_aris.log 2>&1 || error_exit "Error reading ARIS data."
-#python3.6 -u benchmark.py > logs/benchmark.log 2>&1 || error_exit "Error executing Benchmark script."
+cd /home/ahfc/fnsb-benchmark
+python3.6 -u read_aris.py > logs/read_aris.log 2>&1 || error_exit "Error reading ARIS data."
+python3.6 -u benchmark.py > logs/benchmark.log 2>&1 || error_exit "Error executing Benchmark script."
 
-#rm -rf /home/ahfc/webapps/benchmark/* || error_exit "Error removing old web site files."
-#cp -r /home/ahfc/fnsb-benchmark/output/* /home/ahfc/webapps/benchmark/ || error_exit "Error copying new files to site."
+rm -rf /home/ahfc/webapps/benchmark/* || error_exit "Error removing old web site files."
+cp -r /home/ahfc/fnsb-benchmark/output/* /home/ahfc/webapps/benchmark/ || error_exit "Error copying new files to site."
 
 /usr/bin/sendmail "$recipient" <<EOF
 Subject: AHFC Benchmark Script Succeeded
