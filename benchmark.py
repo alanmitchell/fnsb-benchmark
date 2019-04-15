cb7e62af719ba7917b2e26105bb1abd240df4877
Ben <bloeffler@fnsb.us>
2019-04-15 08:35:02 -0800

    Revert "Revert "Allowed multiple processes to work on site analysis.""
    This reverts commit b08272bbc92684c09b2ec77272d9462795f2e192.

--- benchmark.py
+++ benchmark.py
@@ -44,6 +44,7 @@ import os
 import pprint
 import datetime
 import warnings
+import multiprocessing
 import pandas as pd
 import numpy as np
 import bench_util as bu
@@ -1432,7 +1433,7 @@ def FY_spreadsheets(dfp, ut):
         
         
 
-        # Caclulate EUI, ECI
+        # Calculate EUI, ECI
 
         #Use HDD and SQFT to calculate EUIs and ECI.
         df_FYtotal['eci'] = df_FYtotal.total_energy_cost / df_FYtotal.sq_ft
@@ -1731,17 +1732,15 @@ if __name__=="__main__":
     score_template = template_util.get_template('sites/scorecard.html')
 
 
-    site_count = 0    # tracks number of site processed
-    for site_id in util_obj.all_sites():
+    def run_site(site_id):
         # This line shortens the calculation process to start with whatever
         # Site ID you want to start with
-        # if site_id < '15711': continue
+        # if site_id < '15711': return
 
         msg("Site '{}' is being processed...".format(site_id))
         
         # Generate site specific spreadsheet
         Site_spreadsheets(site_id, df, util_obj)
-
 
         # Gather template data from each of the report sections.  The functions
         # return a dictionary with variables needed by the template.  Sometimes other
@@ -1795,9 +1794,14 @@ if __name__=="__main__":
         with open(f'output/sites/{site_id}_score.html', 'w') as fout:
             fout.write(result)
 
-        site_count += 1
-        if site_count == settings.MAX_NUMBER_SITES_TO_RUN:
-            break
-    
+    # all sites to run
+    site_list = util_obj.all_sites()
+    if settings.MAX_NUMBER_SITES_TO_RUN:
+        # it was requested to run a shorter set of sites, so truncate
+        # the list
+        site_list = site_list[:settings.MAX_NUMBER_SITES_TO_RUN]
+    with multiprocessing.Pool(processes=settings.PROCESS_COUNT) as pool:
+        pool.map(run_site, site_list)
+
     print()
     msg('Benchmarking Script Complete!')
